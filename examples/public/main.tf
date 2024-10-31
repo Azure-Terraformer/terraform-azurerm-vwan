80d46820c1deb2d88e8e923ca37fa0e308e59c88
Mark Tinderholt <mtinderholt@microsoft.com>
2024-10-31 12:37:50 -0400

    tf docs

---
+++ examples/public/main.tf
@@ -0,0 +1,26 @@
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-aztf-core-network"
+  location = "westus3"
+}
+
+module "vwan" {
+
+  source  = "Azure-Terraformer/vwan/azurerm"
+  version = "1.0.0"
+
+  resource_group_name    = azurerm_resource_group.main.name
+  location               = azurerm_resource_group.main.location
+  name                   = "aztf-core-network-${random_string.suffix.result}"
+  primary_address_prefix = "10.8.0.0/23"
+
+  additional_regions = {
+    eastus2 = "10.8.2.0/23"
+  }
+
+}
