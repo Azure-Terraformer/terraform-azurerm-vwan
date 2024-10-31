e729e8ca35dde4a259e570d7c16dbe13e8da6361
Mark Tinderholt <mtinderholt@microsoft.com>
2024-10-31 12:39:39 -0400

    vars

--- variables.tf
+++ variables.tf
@@ -1,15 +1,29 @@
+# Resource Group Name
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "Specifies the name of the resource group in which to create the resources."
 }
+
+# Location
 variable "location" {
-  type = string
+  type        = string
+  description = "Specifies the Azure region where the resources will be deployed (e.g., 'East US')."
 }
+
+# Name
 variable "name" {
-  type = string
+  type        = string
+  description = "Defines the base name to be used for the created resources, helping standardize naming conventions."
 }
+
+# Primary Address Prefix
 variable "primary_address_prefix" {
-  type = string
+  type        = string
+  description = "Specifies the primary IP address prefix (CIDR format) to assign to the main subnet for resources."
 }
+
+# Additional Regions
 variable "additional_regions" {
-  type = map(string)
+  type        = map(string)
+  description = "A map of additional Azure regions where replicated resources will be deployed, with each key as the region name and value as the region-specific configuration."
 }
